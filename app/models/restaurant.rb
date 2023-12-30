24d3a7b98a36dc745b61068e2b49dbe23dd332e7
paola ramirez <paorp2002@gmail.com>
2023-12-29 19:42:43 -0600

    validaciones y asociaciones

---
+++ app/models/restaurant.rb
@@ -0,0 +1,11 @@
+class Restaurant < ApplicationRecord
+  # Asociaciones
+  belongs_to :user
+  has_many :category_restaurants
+  has_many :categories, through: :category_restaurants
+  has_one_attached :photo
+
+  # Validaciones
+  validates :name, :address, length: { minimum: 5 }
+  validates :description, length: {minimum: 15 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_30_011052) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_30_012942) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "category_restaurants", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_category_restaurants_on_category_id"
+    t.index ["restaurant_id"], name: "index_category_restaurants_on_restaurant_id"
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.float "avg_rating"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_restaurants_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
+    t.integer "rating"
+    t.bigint "restaurant_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +65,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_30_011052) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "category_restaurants", "categories"
+  add_foreign_key "category_restaurants", "restaurants"
+  add_foreign_key "restaurants", "users"
+  add_foreign_key "reviews", "restaurants"
+  add_foreign_key "reviews", "users"
 end
